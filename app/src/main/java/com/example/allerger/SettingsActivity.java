cd081ec65c791f4973b79c94fbdb7d2a451ea2d2
HyungKyu Choi <fabric93@naver.com>
2019-05-28 18:05:48 +0900

    Add settings
    preference is available

--- app/src/main/java/com/example/allerger/SettingsActivity.java
+++ app/src/main/java/com/example/allerger/SettingsActivity.java
@@ -1,12 +1,14 @@
 package com.example.allerger;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle bundle){
         super.onCreate(bundle);
-        setContentView(R.layout.settings);
+        addPreferencesFromResource(R.xml.settings);
     }
 }
