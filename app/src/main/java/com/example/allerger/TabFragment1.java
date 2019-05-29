06e8329e0a1511907f1e8b0d8bd4e00683ba7c0b
HyungKyu Choi <fabric93@naver.com>
2019-05-29 20:18:07 +0900

    Set Fragment 1 Using Glide
    gradle is changed

--- app/src/main/java/com/example/allerger/TabFragment1.java
+++ app/src/main/java/com/example/allerger/TabFragment1.java
@@ -5,15 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.RelativeLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
 
 public class TabFragment1 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        RelativeLayout tab1 = (RelativeLayout) inflater.inflate(R.layout.tab_fragment_1, container, false);
+        LinearLayout tab1 = (LinearLayout) inflater.inflate(R.layout.tab_fragment_1, container, false);
+        ImageView fra1 = (ImageView)tab1.findViewById(R.id.frag1_main);
+        Glide.with(this).load(R.drawable.fra).into(fra1);
         return tab1;
     }
 }
