023014c4cfe76c7f6646bb1405a81e5701200ea9
HyungKyu Choi <fabric93@naver.com>
2019-05-31 16:51:03 +0900

    Set Help Activity
    Added 3 Images about help

--- app/src/main/java/com/example/allerger/HelpActivity.java
+++ app/src/main/java/com/example/allerger/HelpActivity.java
@@ -2,6 +2,9 @@ package com.example.allerger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+        ImageView help1 = findViewById(R.id.help1);
+        ImageView help2 = findViewById(R.id.help2);
+        ImageView help3 = findViewById(R.id.help3);
+        Glide.with(this).load(R.drawable.help1).into(help1);
+        Glide.with(this).load(R.drawable.help2).into(help2);
+        Glide.with(this).load(R.drawable.help3).into(help3);
     }
 }
