50e4e7eb243f316cac68ddf2952719c754907103
rurimo <topmazino@gmail.com>
2019-06-12 14:34:03 +0900

    implements image cropping & migrated android x

--- app/src/main/java/com/Allergerapp/allerger/PolicyActivity.java
+++ app/src/main/java/com/Allergerapp/allerger/PolicyActivity.java
@@ -1,6 +1,6 @@
 package com.Allergerapp.allerger;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PolicyActivity extends AppCompatActivity {
