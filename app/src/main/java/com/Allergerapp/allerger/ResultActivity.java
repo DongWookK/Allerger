19d5be0ab5b22f7ce5ed9744a67fd7ac7e73e4fa
ChaJaewook <44600628+ChaJaewook@users.noreply.github.com>
2019-06-12 12:42:06 +0900

    Add language
    Add French language on database.

--- app/src/main/java/com/Allergerapp/allerger/ResultActivity.java
+++ app/src/main/java/com/Allergerapp/allerger/ResultActivity.java
@@ -38,13 +38,15 @@ import java.util.HashMap;
 public class ResultActivity extends AppCompatActivity {
 
     private final String dbName = "webnautes";// DBNAME
-    private final String tableName = "person";
+    //private String tableName = "Allerger_table";
+    public String tableName;
+
     Bitmap imageBitmap;
     ImageView imageView;
 
-    private String names[];
+    private String Food_E[];
     {
-        names = new String[]{"bean", "egg", "shrimp", "peach", "kiwi", "flour", "peanut", "fish", "tomato",
+        Food_E = new String[]{"bean", "egg", "shrimp", "peach", "kiwi", "flour", "peanut", "fish", "tomato",
                 "almond","melon","walnut","hamburger","cheese","salmon","crap","wheat","chocolate","butter","cocoa","canola","soy","albumin","mayonnaise","globulin",
                 "ovalbumin","ovomucin","ovomucoid","livetin","baked","lecithin","macaroni","nougat","pasta",
                 "cream","casein","curds","custard","ghee","lacto","pudding","yogurt","margarine","miso","sprouts","tofu","nuts", "cashews","hazelnuts","ginko", "chestnut",
@@ -52,9 +54,9 @@ public class ResultActivity extends AppCompatActivity {
                 ,"clams","mussel","octopus","oyster","snail","escargot","surimi","abocado","banana"};
     }
 
-    private final String phones[];// COMPARE WITH NAMES THEN SHOW TO USER THE KOREAN
+    private final String Ingredient_E[];// COMPARE WITH NAMES THEN SHOW TO USER THE KOREAN
     {
-        phones = new String[]{"견과류알러지(땅콩)", "계란 알러지(달걀)", "갑각류 알러지(새우)", "과일 알러지(복숭아)","과일 알러지(키위)", "밀가루 알러지(밀)", "견과류 알러지(땅콩)", "어패류 알러지(물고기)", "과일 알러지(토마토)","견과류 알러지(아몬드)","과일 알러지(멜론)","견과류 알러지(호두)",
+        Ingredient_E = new String[]{"견과류알러지(땅콩)", "계란 알러지(달걀)", "갑각류 알러지(새우)", "과일 알러지(복숭아)","과일 알러지(키위)", "밀가루 알러지(밀)", "견과류 알러지(땅콩)", "어패류 알러지(물고기)", "과일 알러지(토마토)","견과류 알러지(아몬드)","과일 알러지(멜론)","견과류 알러지(호두)",
                 "유제품 알러지(햄버거)","유제품 알러지(치즈)","어패류 알러지(연어)","갑각류 알러지(게)","밀가루 알러지(밀)","유제품 알러지(초콜렛)","유제품 알러지(버터)","코코아 알러지"," 콩 알러지(카놀라)","콩 알러지(된장)","계란 알러지","계란 알러지(마요네즈 함유)","계란 알러지",
                 "계란관련 알러지","계란관련 알러지","계란관련 알러지","계란관련 알러지","계란관련 알러지(빵)","계란관련 알러지","계란관련 알러지","계란관련 알러지(누가초콜릿)",
                 "계란관련 알러지(파스타면)","유제품 알러지(크림)","유제품 알러지(카제인)","유제품 알러지","유제품 알러지(커스타드)","유제품 알러지(버터오일)","유제품 알러지",
@@ -64,13 +66,38 @@ public class ResultActivity extends AppCompatActivity {
                 "갑각류 알러지(굴)","달팽이","달팽이","맛살","아보카도","바나나"};
     }
 
-    ArrayList<HashMap<String, String>> personList; //DB CODE
+    private String Food_F[];
+    {
+        Food_F = new String[]{"fèves", "eggufs", "crevettes", "pêche", "kiwi", "farine", "arachide", "poisson", "tomate",
+                "al","melon","walnut","hamburger","fromage","saumon","crap","blé","chocolat","beurre","cacao","canola","soja","albumine","mayonnaise","globuline",
+                "ovalbumine","ovomucine","ovomucoïde","livetin","aufour","lécithine","macaroni","nougat","pâtes",
+                "crème","caséine","cur","custard","ghee","lacto","pudding","yogourt","margarine","miso","lesgermes","tofu","noix", "cajou","noisettes","ginko", "marron",
+                "lichi","lichee","lychee","pacanes","pin","pistachio","pistaches","langouste","scampo", "tomalley","oral"
+                ,"myes","moules","pieuvre","huître","snail","escargot","ron","abocado","banane"};
+    }
+
+
+
+    private final String Ingredient_F[];// COMPARE WITH NAMES THEN SHOW TO USER THE KOREAN
+    {
+        Ingredient_F = new String[]{"견과류알러지(땅콩)", "계란 알러지(달걀)", "갑각류 알러지(새우)", "과일 알러지(복숭아)","과일 알러지(키위)", "밀가루 알러지(밀)", "견과류 알러지(땅콩)", "어패류 알러지(물고기)", "과일 알러지(토마토)","견과류 알러지(아몬드)","과일 알러지(멜론)","견과류 알러지(호두)",
+                "유제품 알러지(햄버거)","유제품 알러지(치즈)","어패류 알러지(연어)","갑각류 알러지(게)","밀가루 알러지(밀)","유제품 알러지(초콜렛)","유제품 알러지(버터)","코코아 알러지"," 콩 알러지(카놀라)","콩 알러지(된장)","계란 알러지","계란 알러지(마요네즈 함유)","계란 알러지",
+                "계란관련 알러지","계란관련 알러지","계란관련 알러지","계란관련 알러지","계란관련 알러지(빵)","계란관련 알러지","계란관련 알러지","계란관련 알러지(누가초콜릿)",
+                "계란관련 알러지(파스타면)","유제품 알러지(크림)","유제품 알러지(카제인)","유제품 알러지","유제품 알러지(커스타드)","유제품 알러지(버터오일)","유제품 알러지",
+                "유제품 알러지(푸딩)", "유제품 알러지(요거트)", "유제품 알러지(마가린)","콩 알러지(미소)","콩 알러지(콩나물)","콩 알러지(두부)","견과류 알러지","견과류 알러지(캐슈넛)","견과류 알러지(헤이즐넛)"
+                , "견과류 알러지(은행)","견과류 알러지(밤)","견과류 알러지(리찌넛)","견과류 알러지(리찌넛)","견과류 알러지(리찌넛)","견과류 알러지(피칸)","견과류 알러지(잣)","견과류 알러지(피스타치오)",
+                "갑각류 알러지(랍스터)","갑각류 알러지(랍스터)","갑각류 알러지(랍스터)","갑각류 알러지(랍스터)", "갑각류 알러지(전복)","갑각류 알러지(조개)","갑각류 알러지(홍합)","해산물 알러지(문어)",
+                "갑각류 알러지(굴)","달팽이","달팽이","맛살","아보카도","바나나"};
+    }
+
+
+
+    ArrayList<HashMap<String, String>> AllergyList; //DB CODE
     ListView list;
-    private static final String TAG_NAME = "name";
-    private static final String TAG_PHONE ="Allergy";
+    private static final String TAG_FOOD = "Food";
+    private static final String TAG_INGREDIENT ="Allergy";
 
     SQLiteDatabase sampleDB = null;
-    //ListAdapter adapter;
     ListViewAdapter adapter;
 
     Bitmap image; // 사용되는 이미지
@@ -94,16 +121,29 @@ public class ResultActivity extends AppCompatActivity {
 
             //테이블이 존재하지 않으면 새로 생성합니다.
             sampleDB.execSQL("CREATE TABLE IF NOT EXISTS " + tableName
-                    + " (name VARCHAR(20), phone VARCHAR(20) );");
+                    + " (Food VARCHAR(20), Ingredient VARCHAR(20) );");
 
             //테이블이 존재하는 경우 기존 데이터를 지우기 위해서 사용합니다.
             sampleDB.execSQL("DELETE FROM " + tableName  );
+            int check=1;
+
+            if(check==1)// 선택하는 거에 따라 E가 붙은게 english 고 F붙은게 프랑스어
+            {
+                for (int i=0; i<Food_E.length; i++ ) {
+                    sampleDB.execSQL("INSERT INTO " + tableName
+                            + " (Food, Ingredient)  Values ('" + Food_E[i] + "', '" + Ingredient_E[i]+"');");
+                }
+            }
+            else if(check==2)
+            {
+                for (int i=0; i<Food_F.length; i++ ) {
+                    sampleDB.execSQL("INSERT INTO " + tableName
+                            + " (Food, Ingredient)  Values ('" + Food_F[i] + "', '" + Ingredient_F[i]+"');");
+                }
+            }
 
             //새로운 데이터를 테이블에 집어넣습니다..
-            for (int i=0; i<names.length; i++ ) {
-                sampleDB.execSQL("INSERT INTO " + tableName
-                        + " (name, phone)  Values ('" + names[i] + "', '" + phones[i]+"');");
-            }
+
 
             sampleDB.close();//close DB
 
@@ -114,18 +154,6 @@ public class ResultActivity extends AppCompatActivity {
 
         }
 
-
-
-
-        /*
-        Intent intent = getIntent();
-        String path = intent.getExtras().getString("path");
-        TextView textView = findViewById(R.id.pathId);
-        textView.setText(path);  // 텍스트를 경로로 변경
-
-        File imgFile = new File(path);
-        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-        */
         int code;
         Intent data = getIntent();
         Bundle extras = data.getExtras();
@@ -280,16 +308,13 @@ public class ResultActivity extends AppCompatActivity {
                 if (c.moveToFirst()) {
                     do {
 
-                        String Name = c.getString(c.getColumnIndex("name"));
-                        String Phone = c.getString(c.getColumnIndex("phone"));
-                        HashMap<String,String> persons = new HashMap<String,String>();
+                        String Name = c.getString(c.getColumnIndex("Food"));
+                        String Phone = c.getString(c.getColumnIndex("Ingredient"));
+                        HashMap<String,String> Allergy = new HashMap<String,String>();
                         int pic_id=0;
                         if (Clearing.contains(Name))
                         {
 
-                            /*persons.put(TAG_NAME,Name);
-                            persons.put(TAG_PHONE,Phone);
-                            personList.add(persons);*/
                             if(Phone.contains("계란"))
                             {
                                 pic_id=R.drawable.eggs;
